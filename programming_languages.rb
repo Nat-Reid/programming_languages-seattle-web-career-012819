c1b22fc42c9194d1adc8756fdfe65e6350c2c997
Nat-Reid <nathanielreid@gmail.com>
2019-01-13 22:33:40 -0800

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,13 @@
-def reformat_languages(languages)
-  # your code here
+require 'pry'
+
+def reformat_languages(languages_hash)
+  formatted_hash = {}
+  languages_hash.each do |style, languages|
+    languages.each do |language, type_hash|
+      #binding.pry
+      formatted_hash[language] = {:style => [style], :type => type_hash[:type]}
+    end
+  end
+  formatted_hash[:javascript][:style] << :oo
+  formatted_hash
 end
